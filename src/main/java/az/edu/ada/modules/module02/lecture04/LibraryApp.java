91025ce7f7b4867073b46eb79ad4d586bef9f94a
Elvin Taghizade <elvintaghiyev184@gmail.com>
2026-02-03 09:02:12 +0400

    lesson04 class code attached

---
+++ src/main/java/az/edu/ada/modules/module02/lecture04/LibraryApp.java
@@ -0,0 +1,12 @@
+package az.edu.ada.modules.module02.lecture04;
+
+public class LibraryApp {
+
+    public void saveBook() {
+        // fetch from db: 1
+
+        // do internal process
+
+        // save into db: 2
+    }
+}
